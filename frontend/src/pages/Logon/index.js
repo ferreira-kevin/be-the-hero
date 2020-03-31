c2080dca79478c8e349c8af3d147bfe9d89cbb50
kevin.ferreira <kevin.ferreira@mrv.com.br>
2020-03-31 12:16:58 -0300

    front end

---
+++ frontend/src/pages/Logon/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import './style.css';
+import { FiLogIn } from 'react-icons/fi'
+import logoImg from '../../assets/logo.svg'
+import heroesImg from '../../assets/heroes.png';
+
+export default function Logon(){
+    return(
+        <div className="logon-container">
+            <section className="form">
+                <img src={logoImg} alt="Logo"/>
+
+                <form action="">
+                    <h1>Faça seu logon</h1>
+                    <input placeholder="Sua ID" />
+                    <button type="submit">Entrar</button>
+                    <a href="/register">
+                        < FiLogIn size={16} color="#E02041"/>
+                        Não tenho cadastro
+                    </a>
+                </form>
+            </section>
+            <img src={heroesImg} alt="Heroes"/>
+        </div>
+    );
+}
